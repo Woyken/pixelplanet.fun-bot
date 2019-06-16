f6c3ad6cfb4511c72f090ab087e6109c6d8ee8f6
Karolis <karolis.uzkuraitis@gmail.com>
2019-06-16 22:36:34 +0300

    fixes #18

--- src/guid.ts
+++ src/guid.ts
@@ -11,7 +11,7 @@ export class Guid {
 
     public static validateGuid(guid: string): boolean {
         const matched = guid.match(
-            /^[0-9a-f]{8}[0-9a-f]{4}[1-5][0-9a-f]{3}[0-9a-f]{4}[0-9a-f]{12}$/i,
+            /^[0-9a-f]{8}[0-9a-f]{4}[0-9a-f]{4}[0-9a-f]{4}[0-9a-f]{12}$/i,
         );
         return matched !== null;
     }
