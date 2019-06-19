e0918258b6679ac06c97813c8c9a0ba49576964f
David <davidsh13@gmail.com>
2019-06-19 17:01:57 +0300

    fixes #20

--- src/colorConverter.ts
+++ src/colorConverter.ts
@@ -2,30 +2,37 @@ class ColorConverter {
     private colorDict: { [id: number]: [number, number, number]; } = {};
 
     constructor() {
-        this.colorDict[2] = this.parseColor('#ffffff');
-        this.colorDict[3] = this.parseColor('#e4e4e4');
-        this.colorDict[4] = this.parseColor('#888888');
-        this.colorDict[5] = this.parseColor('#4e4e4e');
-        this.colorDict[6] = this.parseColor('#000000');
-        this.colorDict[7] = this.parseColor('#f4b3ae');
-        this.colorDict[8] = this.parseColor('#ffa7d1');
-        this.colorDict[9] = this.parseColor('#ff6565');
-        this.colorDict[10] = this.parseColor('#e50000');
-        this.colorDict[11] = this.parseColor('#fea460');
-        this.colorDict[12] = this.parseColor('#e59500');
-        this.colorDict[13] = this.parseColor('#a06a42');
-        this.colorDict[14] = this.parseColor('#f5dfb0');
-        this.colorDict[15] = this.parseColor('#e5d900');
-        this.colorDict[16] = this.parseColor('#94e044');
-        this.colorDict[17] = this.parseColor('#02be01');
-        this.colorDict[18] = this.parseColor('#006513');
-        this.colorDict[19] = this.parseColor('#cae3ff');
-        this.colorDict[20] = this.parseColor('#00d3dd');
-        this.colorDict[21] = this.parseColor('#0083c7');
-        this.colorDict[22] = this.parseColor('#0000ea');
-        this.colorDict[23] = this.parseColor('#191973');
-        this.colorDict[24] = this.parseColor('#cf6ee4');
-        this.colorDict[25] = this.parseColor('#820080');
+        // * = new
+        this.colorDict[2] = this.parseColor('#ffffff'); // white
+        this.colorDict[3] = this.parseColor('#e4e4e4'); // light gray
+        this.colorDict[4] = this.parseColor('#c4c4c4'); // silver*
+        this.colorDict[5] = this.parseColor('#888888'); // dark gray
+        this.colorDict[6] = this.parseColor('#4e4e4e'); // darker gray
+        this.colorDict[7] = this.parseColor('#000000'); // black
+        this.colorDict[8] = this.parseColor('#f4b3ae'); // skin
+        this.colorDict[9] = this.parseColor('#ffa7d1'); // light pink
+        this.colorDict[10] = this.parseColor('#ff54b2'); // pink*
+        this.colorDict[11] = this.parseColor('#ff6565'); // peach
+        this.colorDict[12] = this.parseColor('#e50000'); // red
+        this.colorDict[13] = this.parseColor('#9a0000'); // dark red*
+        this.colorDict[14] = this.parseColor('#fea460'); // light brown
+        this.colorDict[15] = this.parseColor('#e59500'); // orange
+        this.colorDict[16] = this.parseColor('#a06a42'); // brown
+        this.colorDict[17] = this.parseColor('#604028'); // dark brown*
+        this.colorDict[18] = this.parseColor('#f5dfb0'); // sand
+        this.colorDict[19] = this.parseColor('#fff889'); // khaki*
+        this.colorDict[20] = this.parseColor('#e5d900'); // yellow
+        this.colorDict[21] = this.parseColor('#94e044'); // light green
+        this.colorDict[22] = this.parseColor('#02be01'); // green
+        this.colorDict[23] = this.parseColor('#688338'); // olive*
+        this.colorDict[24] = this.parseColor('#006513'); // dark green
+        this.colorDict[25] = this.parseColor('#cae3ff'); // sky blue
+        this.colorDict[26] = this.parseColor('#00d3dd'); // light blue
+        this.colorDict[27] = this.parseColor('#0083c7'); // dark blue
+        this.colorDict[28] = this.parseColor('#0000ea'); // blue
+        this.colorDict[29] = this.parseColor('#191973'); // darker blue
+        this.colorDict[30] = this.parseColor('#cf6ee4'); // light violette
+        this.colorDict[31] = this.parseColor('#820080'); // violette
     }
 
     public convertActualColor(r: number, g: number, b: number): number {
